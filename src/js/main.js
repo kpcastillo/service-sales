9ee6444cf6a5e4ee03c93b69a12a6e81e38b528c
kpcastillo <krn_iveth09@hotmail.com>
2025-09-30 20:15:18 -0700

    added map and address validator

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,17 @@
 
+import { initMap } from "./address";
 const helloNode = document.createElement("h1");
 
 document.getElementById('content').appendChild(helloNode);
 helloNode.textContent = "Hello from JavaScript!";
 
+initMap();
+//import { initMap } from "./address.js";
+
+import { APILoader } from "@googlemaps/js-api-loader";
+
+const CONFIGURATION = {
+  "ctaTitle": "Checkout",
+  "mapOptions": {"center":{"lat":37.4221,"lng":-122.0841},"fullscreenControl":false,"mapTypeControl":false,"streetViewControl":false,"zoom":16,"zoomControl":true,"maxZoom":22,"mapId":""},
+  "mapsApiKey": "AIzaSyALbB99cOQM6d4WIgh4-Vunctftsal7ZSY"
+};
