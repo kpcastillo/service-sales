b4c3b03d32689d16ad41079a7ab19316b93667d0
kpcastillo <krn_iveth09@hotmail.com>
2025-10-06 18:30:58 -0700

    styles added pages html fixed

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 
 import { showPosition, loadHeaderFooter } from './utils.js';  
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 showPosition();
 const helloNode = document.createElement("h1");
@@ -8,5 +9,4 @@ document.getElementById('content').appendChild(helloNode);
 helloNode.textContent = "Hello from JavaScript!";
 
 //Header and footer loading
-
  window.addEventListener('DOMContentLoaded', () => loadHeaderFooter());
