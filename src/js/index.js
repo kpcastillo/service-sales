905ed2c8b4ad4d6069b5aceb74d2483bf47e047a
kpcastillo <krn_iveth09@hotmail.com>
2025-10-08 14:25:04 -0700

    form js

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,5 @@
 
 import { showPosition, loadHeaderFooter, showSlides } from './utils.js';
-import {loadGoogleMaps, initAutocomplete,renderPreview, saveLocal} from './address.js';
-
-// Load Google Maps API and initialize autocomplete
-const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-loadGoogleMaps(apiKey).then(() => {
-  const addressInput = document.getElementById('address');
-  initAutocomplete(addressInput, (place) => {
-    console.log('Selected place:', place);
-  });
-});
-renderPreview(payload);
-
-alert('Saved!');
-
-printBtn.addEventListener('click', () => window.print());
 
 // Show user position
 showPosition();
--- src/js/masonry.js
+++ src/js/masonry.js
@@ -1,3 +1,18 @@
 import {loadHeaderFooter} from './utils.js';
+import {loadGoogleMaps, initAutocomplete,renderPreview, saveLocal} from './address.js';
+
+// Load Google Maps API and initialize autocomplete
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+loadGoogleMaps(apiKey).then(() => {
+  const addressInput = document.getElementById('address');
+  initAutocomplete(addressInput, (place) => {
+    console.log('Selected place:', place);
+  });
+});
+renderPreview(payload);
+
+alert('Saved!');
+
+printBtn.addEventListener('click', () => window.print());
 
 loadHeaderFooter();
