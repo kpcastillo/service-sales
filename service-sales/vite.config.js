e387434377d2abff781845bbd1fe9412324f553e
kpcastillo <krn_iveth09@hotmail.com>
2025-10-13 23:27:45 -0700

    fixed paths

--- service-sales/vite.config.js
+++ service-sales/vite.config.js
@@ -6,23 +6,14 @@ const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
   build: {
-    lib: {
-      entry: './lib/main.js',
-      name: 'Counter',
-      fileName: 'counter',
-    },
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        masonry: resolve(__dirname, 'masonry.html'),
-        pavers: resolve(__dirname, 'pavers.html'),
-        concrete: resolve(__dirname, 'concrete.html'),
+        masonry: resolve(__dirname, 'pages/masonry.html'),
+        pavers: resolve(__dirname, 'pages/pavers.html'),
+        concrete: resolve(__dirname, 'pages/concrete.html'),
       }
   },
-  redirects: {
-    '/masonry.html': '/masonry',
-    '/pavers.html': '/pavers',
-    '/concrete.html': '/concrete',
-  }
+  },
 });
 
